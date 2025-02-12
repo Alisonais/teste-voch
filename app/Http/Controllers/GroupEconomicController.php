5e22d65047a63b8ba2109bee451f2f0620d4138a
AlisonAis <alisonais@ymail.com>
2025-02-11 21:57:20 -0300

    refactor: remove import

--- app/Http/Controllers/GroupEconomicController.php
+++ app/Http/Controllers/GroupEconomicController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Requests\GroupEconomicRequest;
 use App\Models\GroupEconomic;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class GroupEconomicController extends Controller
